b3d8f4c45f3ebd0d4438409bc489bac5d9430ff7
elroypedrokame <elroypedrokameo10@gmail.com>
2024-06-24 01:17:54 +0700

    fix: product detail width

--- src/components/DetailProduct/[id].tsx
+++ src/components/DetailProduct/[id].tsx
@@ -67,7 +67,7 @@ function DetailProduct() {
   return (
     <>
       <Header />
-      <div className="m-auto my-5 max-w-container flex gap-5">
+      <div className="m-auto my-5 max-w-container flex justify-between gap-5">
         <div>
           <img src={product.productImage} alt={product.productName} />
         </div>
@@ -91,7 +91,7 @@ function DetailProduct() {
             <p>{product.productDetail}</p>
           </div>
         </div>
-        <div className="rounded-lg product-card p-5 h-max max-w-[200px]">
+        <div className="rounded-lg product-card p-5 h-max max-w-[250px]">
           <h3 className="font-bold text-base">Atur Jumlah</h3>
           <div className="mt-4">
             <Counter count={count} productStock={product.productStock} increment={increment} decrement={decrement} />
